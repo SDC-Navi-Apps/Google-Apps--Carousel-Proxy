d48bed56b7c42b2baffad65f80b54883644b364e
Mason Borchard <masonborchard@gmail.com>
2020-03-25 12:18:53 -0700

    Get all 4 services to render to screen

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, '/../public')));
-
+// app.use(express.static(__dirname + '/../client'));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
@@ -12,29 +12,29 @@ app.listen(port, () => {
 
 // get all bundle.js
 
-app.get('./bundle.js/:3001', (req, res) => {
-  request('http://localhost:3002/bundle.js', (err, response, body) => {
-      res.status(200).send(body);
-  })
-})
+// app.get('./bundle.js/:3001', (req, res) => {
+//   request('http://localhost:3001/dist/bundle.js', (err, response, body) => {
+//       res.status(200).send(body);
+//   })
+// })
 
-app.get('./bundle.js/:3002', (req, res) => {
-  request('http://localhost:3002/bundle.js', (err, response, body) => {
-      res.status(200).send(body);
-  })
-})
+// app.get('./bundle.js/:3002', (req, res) => {
+//   request('http://localhost:3002/bundle.js', (err, response, body) => {
+//       res.status(200).send(body);
+//   })
+// })
 
 
-app.get('./bundle.js/:3003', (req, res) => {
-  request('http://localhost:3002/bundle.js', (err, response, body) => {
-      res.status(200).send(body);
-  })
-})
+// app.get('./bundle.js/:3003', (req, res) => {
+//   request('http://localhost:3003/bundle.js', (err, response, body) => {
+//       res.status(200).send(body);
+//   })
+// })
 
-app.get('./bundle.js/:3004', (req, res) => {
-  request('http://localhost:3002/bundle.js', (err, response, body) => {
-      res.status(200).send(body);
-  })
-})
+// app.get('./bundle.js/:3004', (req, res) => {
+//   request('http://localhost:3004/bundle.js', (err, response, body) => {
+//       res.status(200).send(body);
+//   })
+// })
 
 
